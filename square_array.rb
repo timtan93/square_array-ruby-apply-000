7d86746bb5cf1971d8b0a6a3590a7818f554878a
Timothy Tan <timtan93@gmail.com>
2018-11-21 19:50:28 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
   # your code here
-  newarray=[]
-  array each.do |x|
+  newarray = []
+  array.each do |x|
     newarray.push x*x
+  end
+  return newarray
 end
-return newarray
-end
+ 
